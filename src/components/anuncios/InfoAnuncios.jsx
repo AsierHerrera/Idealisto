95d7afeade7c29ef38a2dbb730ecee1aa20edb0b
Asier <asierherrera97@gmail.com>
2024-06-03 20:24:01 +0200

    Puedes hacer click en la tarjeta y te da toda la info, con boton de volver

--- src/components/anuncios/InfoAnuncios.jsx
+++ src/components/anuncios/InfoAnuncios.jsx
@@ -1,22 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PaginaAlojamiento from "../PaginaAlojamiento/PaginaAlojamiento";
 
-const InfoAnuncios = ({ anuncios }) => {
+const InfoAnuncios = ({ anuncios, togglePaginacion }) => {
+  const [alojamientoSeleccionado, setAlojamientoSeleccionado] = useState(null);
+
+  const abrirPaginaAlojamiento = (anuncio) => {
+    console.log("Anuncio seleccionado:", anuncio);
+    setAlojamientoSeleccionado(anuncio);
+    togglePaginacion(false); // Ocultar la paginación al abrir la página del alojamiento
+  };
+
+  const cerrarPaginaAlojamiento = () => {
+    setAlojamientoSeleccionado(null);
+    togglePaginacion(true); // Mostrar la paginación al cerrar la página del alojamiento
+  };
+
+  useEffect(() => {
+    console.log("Estado después de setAlojamientoSeleccionado:", alojamientoSeleccionado);
+  }, [alojamientoSeleccionado]);
+
   return (
     <>
-      {anuncios.map((anuncio, index) => (
-        <article className="task" key={index}>
-          <h3>{anuncio.title}</h3>
-          <p>Precio: {anuncio.price}</p>
-          <p>Superficie: {anuncio.surface}</p>
-          <p>Dormitorios: {anuncio.bedrooms}</p>
-          <p>Cuartos de baño: {anuncio.restrooms}</p>
-          <p>Resumen: {anuncio.features}</p>
-          <p>Descripcion: {anuncio.description}</p>
-          <p>Provincia: {anuncio.location_name}</p>
-        </article>
-      ))}
+      {alojamientoSeleccionado === null ? (
+        anuncios.map((anuncio, index) => (
+          <article className="anuncio" key={index}>
+            <h3>{anuncio.title}</h3>
+            <p>Precio: {anuncio.price}</p>
+            <p>Superficie: {anuncio.surface}</p>
+            <p>Dormitorios: {anuncio.bedrooms}</p>
+            <p>Cuartos de baño: {anuncio.restrooms}</p>
+            <p>Resumen: {anuncio.features}</p>
+            <p>Descripcion: {anuncio.description}</p>
+            <p>Provincia: {anuncio.location_name}</p>
+            <button onClick={() => abrirPaginaAlojamiento(anuncio)}>Más información</button>
+          </article>
+        ))
+      ) : (
+        <PaginaAlojamiento anuncio={alojamientoSeleccionado} onClose={cerrarPaginaAlojamiento} />
+      )}
     </>
   );
-}
+};
 
 export default InfoAnuncios;
